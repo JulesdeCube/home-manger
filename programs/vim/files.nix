d55c21ad61db9be180406d7509864ca551ebbaae
Jules Lefebvre <jules.lefebvre@epita.fr>
2022-03-20 10:49:43 +0100

    ADD: program: vim: python color shema

--- programs/vim/files.nix
+++ programs/vim/files.nix
@@ -1,4 +1,8 @@
 {
+  vim_python_syntax = {
+    source = ./src/python.vim;
+    target = ".vim/syntax/python.vim";
+  };
   vim_c_syntax = {
     source = ./src/c.vim;
     target = ".vim/syntax/c.vim";
